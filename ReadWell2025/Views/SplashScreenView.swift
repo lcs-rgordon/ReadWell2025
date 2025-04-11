6f47d516b58ef2a084d03d19540630d04f9718f9
Russell Gordon <rgordon@lcs.on.ca>
2025-04-11 18:56:07 -0400

    Added a splash screen.

--- ReadWell2025/Views/SplashScreenView.swift
+++ ReadWell2025/Views/SplashScreenView.swift
@@ -9,13 +9,48 @@ import SwiftUI
 
 struct SplashScreenView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            
+            // First layer
+            Color.readWellBrown
+                .ignoresSafeArea()
+            
+            // Second layer
+            VStack {
+                Spacer()
+                Text("ReadWell")
+                    .foregroundStyle(Color.white)
+                    .font(
+                        .custom(
+                            "Papyrus",
+                            size: 64.0,
+                            relativeTo: .largeTitle
+                        )
+                    )
+                    .rotationEffect(
+                        Angle(
+                            degrees: -5
+                        ),
+                        anchor: .center
+                    )
+                
+                Spacer()
+                
+                Image("ReadingJournal")
+                    .resizable()
+                    .scaledToFit()
+                
+                Spacer()
+                
+                Text("Get Started")
+                    .foregroundStyle(Color.white)
+                    .font(.largeTitle)
+                    .bold()
+                
+                Spacer()
+            }
+            
         }
-        .padding()
     }
 }
 
