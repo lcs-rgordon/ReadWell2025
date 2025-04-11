c616d27de4570be23048505b2534282c67c10f40
Russell Gordon <rgordon@lcs.on.ca>
2025-04-11 18:48:03 -0400

    Initial Commit

---
+++ ReadWell2025/ReadWell2025App.swift
@@ -0,0 +1,17 @@
+//
+//  ReadWell2025App.swift
+//  ReadWell2025
+//
+//  Created by Russell Gordon on 2025-04-11.
+//
+
+import SwiftUI
+
+@main
+struct ReadWell2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
